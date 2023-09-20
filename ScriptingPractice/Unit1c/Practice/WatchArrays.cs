326a5d7d4e01a2ca96f0af2747fc259812f560b2
Chance Willmore <chancicus02@gmail.com>
2023-09-19 21:28:14 -0600

    Practicing some more

---
+++ ScriptingPractice/Unit1c/Practice/WatchArrays.cs
@@ -0,0 +1,11 @@
+using System;
+					
+public class Program
+{
+	public void Main()
+	{
+		string[] games = {"Bloodborne", "Mortal Kombat", "Castlevania"};
+		Console.WriteLine(games.Length);
+		Console.WriteLine(games[0]);
+	}
+}
